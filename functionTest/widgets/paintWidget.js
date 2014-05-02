1df88afef9a8769c5dd35714f8a5048593367b4b
atweb-project <dikostaras@yahoo.gr>
2014-05-02 08:46:45 +0300

    v1

--- functionTest/widgets/paintWidget.js
+++ functionTest/widgets/paintWidget.js
@@ -7,98 +7,13 @@
         MyWidget.call(this, pid, ptype)
 
        //Added for paint tool
-         var canvas;
-    var context;
-    var canvasWidth = 490;
-    var canvasHeight = 220;
-    var padding = 25;
-    var lineWidth = 8;
-        
-	        /*var canvas;
-        	var context;
-        	//var canvasWidth = 490;
-        	//var canvasHeight = 220;
-        	var padding = 25;
-        	var lineWidth = 8;
-        	var inEditingMode = false
-        	*/
-        	/*
-        	document.addEventListener( "DOMContentLoaded", function(){
-        		this.prepareCanvas()
-        		
-        	}, false );
-        	*/
-        //end of addition 
-       /* var ctx, color = "#000";
-        var drawTouch = function() {
-			var start = function(e) {
-			ctx.beginPath();
-			x = e.changedTouches[0].pageX;
-			y = e.changedTouches[0].pageY-44;
-			ctx.moveTo(x,y);
-			};
-			var move = function(e) {
-			e.preventDefault();
-			x = e.changedTouches[0].pageX;
-			y = e.changedTouches[0].pageY-44;
-			ctx.lineTo(x,y);
-			ctx.stroke();
-			};
-			document.getElementById("canvas").addEventListener("touchstart", start, false);
-			document.getElementById("canvas").addEventListener("touchmove", move, false);
-			};
-			    
-			// prototype to start drawing on pointer(microsoft ie) using canvas moveTo and lineTo
-			var drawPointer = function() {
-			var start = function(e) {
-			        e = e.originalEvent;
-			ctx.beginPath();
-			x = e.pageX;
-			y = e.pageY-44;
-			ctx.moveTo(x,y);
-			};
-			var move = function(e) {
-			e.preventDefault();
-			        e = e.originalEvent;
-			x = e.pageX;
-			y = e.pageY-44;
-			ctx.lineTo(x,y);
-			ctx.stroke();
-			    };
-			document.getElementById("canvas").addEventListener("MSPointerDown", start, false);
-			document.getElementById("canvas").addEventListener("MSPointerMove", move, false);
-			};
 
-			// prototype to start drawing on mouse using canvas moveTo and lineTo
-			var drawMouse = function() {
-			var clicked = 0;
-			var start = function(e) {
-			clicked = 1;
-			ctx.beginPath();
-			x = e.pageX;
-			y = e.pageY-44;
-			ctx.moveTo(x,y);
-			};
-			var move = function(e) {
-			if(clicked){
-			x = e.pageX;
-			y = e.pageY-44;
-			ctx.lineTo(x,y);
-			ctx.stroke();
-			}
-			};
-			var stop = function(e) {
-			clicked = 0;
-			};
-			document.getElementById("canvas").addEventListener("mousedown", start, false);
-			document.getElementById("canvas").addEventListener("mousemove", move, false);
-			document.addEventListener("mouseup", stop, false);
-			};*/
+	       
        
         this.createElement= function (param) {
         	
-        	var paintelement = '<div id="canvasDiv"></div>'
-        	// var url = "canvas/test.html"
+        	//var paintelement = '<div id="canvasDiv"></div>'
+        	 var url = "canvas/paint1.html"
             
 
 
@@ -109,125 +24,41 @@
             }
 
            //alert(this.getId());
-            return '<div id="' + this.getId()  +  '" style="width:100%; height:100%">' +  paintelement +  '</div>'
-        	 //return '<iframe width="100%" id="' + this.getId() + '" height="100%" src="' + url + '" frameborder="0"  ></iframe>'
+            //return '<div id="' + this.getId()  +  '" style="width:100%; height:100%">' +  paintelement +  '</div>'
+        	 return '<iframe width="100%" id="' + this.getId() + '" height="100%" src="' + url + '" frameborder="0"  ></iframe>'
             
         },
         
 
 		this.initElement = function(param){
-        	//this.prepareCanvas()
-        	//if(appGlobals.designMode= false){
-        		//alert(true)
-        	/*if(appGlobals.isInDesignMode()== false){ 
-        		this.prepareCanvas()
-        	}*/
-        	this.previewInit()
+        	linewidth = $('#linewidth').val();
 
 		},
+		
+		this.selectColor = function(el){
+		    for(var i=0;i<document.getElementsByClassName("palette").length;i++){
+		        document.getElementsByClassName("palette")[i].style.borderColor = "#777";
+		        document.getElementsByClassName("palette")[i].style.borderStyle = "solid";
+		    }
+		    el.style.borderColor = "#fff";
+		    el.style.borderStyle = "dashed";
+		    color = window.getComputedStyle(el).backgroundColor;
+		    //alert(color);
+		    //window.frames[0].window.ctx.beginPath();
+		    //window.frames[0].window.ctx.strokeStyle = color;
+		},
+		
+		this.selectSize = function(){
+			linewidth = $('#linewidth').val();
+			//alert(linewidth);
+			
+		}
 
       
 		
 		
 		this.prepareCanvas = function(){
-        			/*$('#canvasDiv').html('');
-        			
-        		 	var canvasDiv = document.getElementById('canvasDiv');
-                	canvas = document.createElement('canvas');
-                	//canvas.setAttribute('width', '100%');
-                	//canvas.setAttribute('height', '100%');
-                	canvas.setAttribute('id', 'canvas');
-                	  
-                	canvasDiv.appendChild(canvas);
-                	//$('#canvasDiv').append('<span>Hey</span>');
-
-                	if(typeof G_vmlCanvasManager != 'undefined') {
-                		canvas = G_vmlCanvasManager.initElement(canvas);
-                	}
-                	canvas = document.getElementById('canvas');
-                	context = canvas.getContext("2d");
-        			
-                	// Add mouse events
-                	alert('yes');
-                	
-                	var clickX = new Array();
-                	var clickY = new Array();
-                	var clickDrag = new Array();
-                	var paint;
-                	
-                	$('#canvas').mousedown(function(e){
-                		//alert('mousedown');
-                		  var mouseX = e.pageX - this.offsetLeft;
-                		  var mouseY = e.pageY - this.offsetTop;
-                				
-                		  paint = true;
-                		  //alert(paint)
-                		  addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-                		  redraw();
-                		});
-                	
-                	$('#canvas').mousemove(function(e){
-                		//alert('mousemove');
-                		  if(paint){
-                		    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-                		    redraw();
-                		  }
-                		});
-                	
-                	$('#canvas').mouseup(function(e){
-                		//alert('mouseup');
-                		  paint = false;
-                		});
-                	
-                	$('#canvas').mouseleave(function(e){
-                		//alert('mouseleave');
-                		  paint = false;
-                		});
-                	
-                	function addClick(x, y, dragging)
-                	{
-                	  clickX.push(x);
-                	  clickY.push(y);
-                	  clickDrag.push(dragging);
-                	}
-
-                	function redraw(){
-                	  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-                	  
-                	  context.strokeStyle = "#df4b26";
-                	  alert(context.strokeStyle)
-                	  context.lineJoin = "round";
-                	  context.lineWidth = 5;
-                	            
-                	  for(var i=0; i < clickX.length; i++) {        
-                	    context.beginPath();
-                	    if(clickDrag[i] && i){
-                	      context.moveTo(clickX[i-1], clickY[i-1]);
-                	     }else{
-                	       context.moveTo(clickX[i]-1, clickY[i]);
-                	     }
-                	     context.lineTo(clickX[i], clickY[i]);
-                	     context.closePath();
-                	     context.stroke();
-                	  }
-                	}*/
-			
-			
-			//document.getElementById("canvasDiv").style.height = window.innerHeight-90;
-		  /*  var canvas = '<canvas id="canvas" width="'+window.innerWidth+'" height="'+(window.innerHeight-90)+'"></canvas>';
-		    document.getElementById("canvasDiv").innerHTML = canvas;
-		    
-		    // setup canvas
-		ctx=document.getElementById("canvas").getContext("2d");
-		ctx.strokeStyle = color;
-		ctx.lineWidth = 5;	
-
-		// setup to trigger drawing on mouse or touch
-		    drawTouch();
-		    drawPointer();
-		    drawMouse();
-
-        	*/
+        		alert('hey');	
 		
 
 
@@ -235,72 +66,7 @@
 		},
 		
 		this.previewInit = function() {
-	        var canvasDiv = document.getElementById('canvasDiv');
-	        canvas = document.createElement('canvas');
-	        canvas.setAttribute('width', canvasWidth);
-	        canvas.setAttribute('height', canvasHeight);
-	        canvas.setAttribute('id', 'canvas');
-	        canvasDiv.appendChild(canvas);
-	        if ( typeof G_vmlCanvasManager != 'undefined') {
-	            canvas = G_vmlCanvasManager.initElement(canvas);
-	        }
-	        context = canvas.getContext("2d");
-
-	        $('#canvas').mousedown(function(e) {
-	            var mouseX = e.pageX - this.offsetLeft;
-	            var mouseY = e.pageY - this.offsetTop;
-
-	            paint = true;
-	            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-	            redraw();
-	        });
-
-	        $('#canvas').mousemove(function(e) {
-	            if (paint) {
-	                addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-	                redraw();
-	            }
-	        });
 	        
-	        $('#canvas').mouseup(function(e){
-	  paint = false;
-	});
-
-	$('#canvas').mouseleave(function(e){
-	  paint = false;
-	});
-
-	var clickX = new Array();
-	var clickY = new Array();
-	var clickDrag = new Array();
-	var paint;
-
-	function addClick(x, y, dragging)
-	{
-	  clickX.push(x);
-	  clickY.push(y);
-	  clickDrag.push(dragging);
-	}
-
-	function redraw(){
-	  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-	  
-	  context.strokeStyle = "#df4b26";
-	  context.lineJoin = "round";
-	  context.lineWidth = 5;
-	            
-	  for(var i=0; i < clickX.length; i++) {        
-	    context.beginPath();
-	    if(clickDrag[i] && i){
-	      context.moveTo(clickX[i-1], clickY[i-1]);
-	     }else{
-	       context.moveTo(clickX[i]-1, clickY[i]);
-	     }
-	     context.lineTo(clickX[i], clickY[i]);
-	     context.closePath();
-	     context.stroke();
-	  }
-	}
 		
 		},
 
@@ -317,8 +83,7 @@
 				//$('#canvasDiv').append('<b>mypreview executed</b>');
 				alert('mypreview');
 			}
-          return { 'paintelement':  $('#'+ this.getId() ).html()
-            	}
+        //  return { 'paintelement':  $('#'+ this.getId() ).html() 	}
             //alert(this.getId());
 			/*var isOpen = $( "#myPreviewDiv" ).dialog( "isOpen" );
             if (isOpen = true){
@@ -366,7 +131,28 @@
 // static variables/functions
 
 PaintWidget.init = function () {
-  //  $("#paintMenu").append("  Text:<input type='text' id='newButtonText'><button onclick='appGlobals.currentObject().changeLabel()'>Update</button>")
+   $("#paintMenu").append("<br>Select a color from the palette<div class='palette-case'>"+
+		"<div class='palette-box'><div class='palette white' onclick='appGlobals.currentObject().selectColor(this)'></div></div>"+
+		"<div class='palette-box'><div class='palette red' onclick='appGlobals.currentObject().selectColor(this)'></div></div>"+
+		"<div class='palette-box'><div class='palette blue' onclick='appGlobals.currentObject().selectColor(this)'></div></div>"+
+		"<div class='palette-box'><div class='palette green' onclick='appGlobals.currentObject().selectColor(this)'></div></div>"+
+		"<div class='palette-box'><div class='palette black' onclick='appGlobals.currentObject().selectColor(this)'></div></div>"+
+		"</div>")
+		
+	$("#paintMenu").append(" <div id='' style=''>"+
+		"<br>Select a line size<br>"+	
+		"<select id='linewidth' onchange='appGlobals.currentObject().selectSize()'>"+
+		"<option value='2'>2</option>"+
+		"<option value='5' selected>5</option>"+
+		"<option value='10'>10</option>"+
+		"<option value='20'>20</option>"+
+		"</select>"+
+		"</div>")
+		
+
+    
+   
+   
 }
 PaintWidget.buttomImage='images/brush_painting.png'
 PaintWidget.typeId= 'Paint'

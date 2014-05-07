ea52bfd245c8e4218f200f201ef288f8f4b50ff5
atweb-project <dikostaras@yahoo.gr>
2014-05-07 09:10:41 +0300

    v2

--- functionTest/widgets/paintWidget.js
+++ functionTest/widgets/paintWidget.js
@@ -7,9 +7,8 @@
         MyWidget.call(this, pid, ptype)
         
         var allowColors = true
-        
-        	//$("iframe").attr('allowcolors')
-        
+        var allowLinewidths = true
+                
         
          this.choiceOfpalette = function()
         {
@@ -20,31 +19,34 @@
 
         },
         
+        this.choiceOfLinewidths = function()
+        {
+
+        	//alert ("button clicked")
+            this.myRegisterUniquePropEvent(  [{ 'prop': 'allowLinewidths', 'ov': allowLinewidths, 'nv': $('#allowLinewidths').prop('checked') }])
+
+
+        },
+        
         this.propChange= function (param) {
 
             // alert ("button prop change "+this.getId()+" param "+param.length)
 
         	 for (var i = 0; i < param.length; i++) {
-
-                // if (param[i].prop == 'url') urlWithoutParam =  param[i].value 
                    
                  if (param[i].prop == 'allowColors') allowColors = param[i].value
-
-                // if( param[i].prop == 'start' ) start = param[i].value
-
-               //  if (param[i].prop == 'end') clipEnd = param[i].value
-
-             //    if( param[i].prop == 'showRel') showRel = param[i].value
-
-             //    if( param[i].prop =='autoplay') autoplay = param[i].value
-
-                     //$('#'+id).attr('allowfullscreen', param[i].value )
+                 
+                 if (param[i].prop == 'allowLinewidths') allowLinewidths = param[i].value
 
              }
         	 
-        	 $('#'+this.getId() ).attr( 'allowColors', allowColors == true ? 1:0  )
         	 allowColorsattribute = allowColors
+        	 allowLinewidthsattribute = allowLinewidths
  				//alert(allowColorsattribute)
+        	 //alert(allowColors)
+        	 $('#'+this.getId() ).attr( 'allowcolors', allowColors == true ? 1:0);
+        	 $('#'+this.getId() ).attr( 'allowlinewidths', allowLinewidths == true ? 1 : 0);
+        	 
         	 $('#'+this.getId() ).attr( 'src', function ( i, val ) { return val; });
 
 
@@ -52,12 +54,9 @@
          
          this.selectionChanged=function()  {
 
-             //  alert(" button selection changed " + $(currentID).html())
-
-              // $('#newButtonText').prop('value', $('#' + this.getId()  ).html())
         	 $('#allowColors').prop('checked', allowColors )
         	 
-                 
+        	 $('#allowLinewidths').prop('checked', allowLinewidths )  
 
             }
 
@@ -67,15 +66,20 @@
           
             if (typeof (param) !== 'undefined') {
 
-                if (typeof (param.url) !== 'undefined') param.url = param.url
+                if (typeof (param.url) !== 'undefined') url = param.url
                 
                 if (typeof (param.allowColors) !== 'undefined') allowColors = param.allowColors
+                
+                if (typeof (param.allowLinewidths) !== 'undefined') allowLinewidths = param.allowLinewidths
 
             }
 
            //alert(this.getId());
         	 allowColorsattribute = allowColors
-        	 return '<iframe width="100%" id="' + this.getId() + '" height="100%" src="' + url + '" frameborder="0"></iframe>'
+        	//alert(allowColorsattribute)
+        	 allowLinewidthsattribute = allowLinewidths
+        	 return '<iframe width="100%" id="' + this.getId() + '" class="gcanvas" height="100%" src="' + url + '" frameborder="0" allowcolors="'+ (allowColors == true ? 1 : 0) +
+        	 '" allowlinewidths="'+ (allowLinewidths == true ? 1 : 0) +'"></iframe>'
         	
         },
         
@@ -83,30 +87,10 @@
         	
 
 		},
-		
-		this.selectColor = function(el){
-			//select marker color
-				    for ( var i = 0; i < document.getElementsByClassName("palette").length; i++) {
-					document.getElementsByClassName("palette")[i].style.borderColor = "#777";
-					document.getElementsByClassName("palette")[i].style.borderStyle = "solid";
-				}
-				el.style.borderColor = "#fff";
-				el.style.borderStyle = "dashed";
-				color = window.getComputedStyle(el).backgroundColor;
-				// alert(color);
-
-		},
-		
-		this.selectSize = function(){
-			//select line size
-			linewidth = $('#linewidth').val();
-			//alert(linewidth);
-			
-		},
-		
+				
         this.createJSON = function() {
-			
-			 return { 'allowColors': allowColors }
+			//alert ("json allowColors " + allowColors )
+			 return { 'allowColors': allowColors,'allowLinewidths':allowLinewidths }
         }
         
          
@@ -115,28 +99,10 @@
 // static variables/functions
 
 PaintWidget.init = function () {
-  /* $("#paintMenu").append("<br>Select a color from the palette<div class='palette-case'>"+
-		"<div class='palette-box'><div class='palette red' onclick='appGlobals.currentObject().selectColor(this)'></div></div>"+
-		"<div class='palette-box'><div class='palette blue' onclick='appGlobals.currentObject().selectColor(this)'></div></div>"+
-		"<div class='palette-box'><div class='palette green' onclick='appGlobals.currentObject().selectColor(this)'></div></div>"+
-		"<div class='palette-box'><div class='palette yellow' onclick='appGlobals.currentObject().selectColor(this)'></div></div>"+
-		"<div class='palette-box'><div class='palette black' onclick='appGlobals.currentObject().selectColor(this)'></div></div>"+
-		"</div>")
-		
-	$("#paintMenu").append(" <div id='' style=''>"+
-		"<br>Select a line size<br>"+	
-		"<select id='linewidth' onchange='appGlobals.currentObject().selectSize()'>"+
-		"<option value='2'>2</option>"+
-		"<option value='5' selected>5</option>"+
-		"<option value='10'>10</option>"+
-		"<option value='20'>20</option>"+
-		"</select>"+
-		"</div>")
-	*/
 	
-	$("#paintMenu").append("<br>I want the user to have a choice of colours<input type='checkbox' id='allowColors' name='allowColors' value='allowColors' onclick='appGlobals.currentObject().choiceOfpalette()'>")
+	$("#paintMenu").append("<br>Allow choice of colours<input type='checkbox' id='allowColors' name='allowColors' value='colors' onclick='appGlobals.currentObject().choiceOfpalette()'>")
 
-    
+    $("#paintMenu").append("<br>Allow choice of line sizes<input type='checkbox' id='allowLinewidths' name='allowLinewidths' value='linewidths' onclick='appGlobals.currentObject().choiceOfLinewidths()'>")
    
    
 }

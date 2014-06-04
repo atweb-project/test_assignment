ae735a617f7c557b453baf0bc1e066502ecabb59
Dimitris Kostaras <dk@out-there-media.com>
2014-06-04 18:06:54 +0300

    rss widget start

--- functionTest/widgets/rssWidget.js
+++ functionTest/widgets/rssWidget.js
@@ -20,13 +20,88 @@
             }
 
            
-            return '<button id="' + this.getId()  +  '" style="width:100%; height:100%">'+ buttonText +'</button>'
+            return '<div id="' + this.getId()  +  '" style="width:100%; height:100%"></div>'
 
+        },
+        
+        this.initElement = function(param) {
+
+			
+			
+			// var rssAnimation = function(e) {
+			//	 $.zazar.ticker({selector: selector})
+			// }
+        	//var selector = '#'+ this.getId() + ' ul'
+        	//var selectedValue = this.getValue()
+        	//alert(selectedValue)
+				
+        	this.getValue()
+	
+
+		
+
+        },
+        
+        this.getValue = function() {
+        	 
+        	
+        	var selector = '#'+ this.getId() + ' ul'
+        	var animationValue =  $('#animation').prop('value')
+        	//alert(animationValue);
+        	
+        	switch (animationValue) { 
+		     case 1 : 
+		    	 $('#'+ this.getId() ).rssfeed('http://feeds.reuters.com/reuters/oddlyEnoughNews',{
+		        		limit: 5,
+		        		header: false,
+		        		titletag: 'div',
+		        		date: false,
+		        		content: false
+		        		}, function(e){
+		        			
+		        			  $.zazar.ticker({selector: selector}) 
+		        		});
+		    	 
+		         break;
+		     case 2 : 
+		    	 
+		    	 $('#'+ this.getId() ).rssfeed('http://feeds.reuters.com/reuters/oddlyEnoughNews',{
+		        		limit: 5,
+		        		header: false,
+		        		titletag: 'div',
+		        		date: false,
+		        		content: false
+		        		}, function(e){
+		        			
+		        			  $.zazar.slider({selector: selector}) 
+		        		});
+			 
+		         break;
+		     case 3 : 
+		    	 
+				 $.zazar.rotate({selector: selector})
+			
+		         break;      
+		     default:
+		    	 $('#'+ this.getId() ).rssfeed('http://feeds.reuters.com/reuters/oddlyEnoughNews',{
+		        		limit: 5,
+		        		header: false,
+		        		titletag: 'div',
+		        		date: false,
+		        		content: false
+		        		});
+		         
+        	}
+        	
+        	
+        	
+        	
+        	
         },
 
         this.createJSON = function() {
 
-            return { 'text': $('#'+ this.getId() ).html() }
+            return {  }
 
         },
         
@@ -66,13 +141,18 @@
 // static variables/functions
 
     RssWidget.init = function () {
-    $("#rssMenu").append("  Text:<input type='text' id='newButtonText'><button onclick='appGlobals.currentObject().changeLabel()'>Update</button>")
+    $("#rssMenu").append("<div><select id='animation' onchange='appGlobals.currentObject().getValue()'>"+
+    		"<option>Choose feed animation</option>"+
+    		"<option value='1'>Ticker</option>"+
+    		"<option value='2'>Slider</option>"+
+    		"<option value='3'>Rotate</option>"+
+    		"</select></div>")
 }
 RssWidget.buttomImage='images/button_icon.png'
 RssWidget.typeId= 'rss'
 RssWidget.myClass= 'widget_rss'
-RssWidget.initialWidth='100'
-RssWidget.initialHeight= '50'
+RssWidget.initialWidth='300'
+RssWidget.initialHeight= '250'
 RssWidget.actionsSectionId='rssMenu'
 
 // not actually  needed??

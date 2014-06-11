7174f085bde56eaba57986cd4011a3ace9d3d768
atweb-project <dikostaras@yahoo.gr>
2014-06-12 00:58:29 +0300

    carousel widget continue

--- functionTest/widgets/carouselWidget.js
+++ functionTest/widgets/carouselWidget.js
@@ -19,7 +19,7 @@
 
             }
             
-         /*   if(Direction = 'vertical'){
+      /*    if(Direction = 'vertical'){
             	return'<div id="' + this.getId()  +  '" width="100%" height="100%" class="flexslider"><ul class="slides">'+
          	   '<li><div><img src="images/carousel/slide1.jpg" /></div><div><img src="images/carousel/slide2.jpg" /></div>'+
         	   '<div><img src="images/carousel/slide3.jpg" /></div><div><img src="images/carousel/slide4.jpg" /></div></li>'+
@@ -27,23 +27,31 @@
        	   '<div><img src="images/carousel/slide3.jpg" /></div><div><img src="images/carousel/slide4.jpg" /></div></li></ul></div>'
             }*/
            
-            return '<div id="' + this.getId()  +  '" width="100%" height="100%" class="flexslider"><ul class="slides">'+
+          /*  return '<div id="' + this.getId()  +  '" width="100%" height="100%" class="flexslider"><ul class="slides">'+
             	   '<li><img src="images/carousel/slide1.jpg" /></li><li><img src="images/carousel/slide2.jpg" /></li>'+
-            	   '<li><img src="images/carousel/slide3.jpg" /></li><li><img src="images/carousel/slide4.jpg" /></li></ul></div>'
+            	   '<li><img src="images/carousel/slide3.jpg" /></li><li><img src="images/carousel/slide4.jpg" /></li></ul></div>'*/
+            
+            return '<div id="' + this.getId()  +  '" class="slideshow" data-cycle-fx=carousel data-cycle-timeout=1000>'+
+            	   '<img src="images/carousel/slide1.jpg" />'+
+            	   '<img src="images/carousel/slide2.jpg" />'+
+            	   '<img src="images/carousel/slide3.jpg" />'+
+            	   '<img src="images/carousel/slide4.jpg" />'+
+            	   '</div><div class="center"><a href="#" id="prev4"><< Prev </a><a href="#" id="next4"> Next >> </a></div>'
 
         },
         
         this.initElement = function(param){
         	//var flexslider;
-        	
-        	$('.flexslider').flexslider({
+        	//if( (appGlobals.isInDesignMode() == false)) 	
+        	/*$('.flexslider').flexslider({
         		animation: "slide",
         	    animationLoop: false,
         		//itemWidth: 150,
         	    //itemMargin: 5,
         	   minItems: 2, //one condition
         	   maxItems: 2,  //second condition
-        	    direction: Direction
+        	    direction: Direction,
+        	    slideshow: false
         	    /*start: function(flexslider){
         	    	 if (flexslider.vars.direction == "vertical"){
         	    		 //alert('yes')
@@ -54,8 +62,18 @@
         	    },*/
         	    //controlNav: "thumbnails"
         	
-        	});
+        	//});
         	
+        	//Cycle2 carousel
+        	$( '.slideshow' ).cycle({
+        		next:"#next4",
+        		prev:"#prev4",
+        		//carouselSlideDimension:"400",
+        		//carouselFluid: true,
+        		//carouselVisible: 2
+        		//carouselVertical:true
+        		//autoHeight: "400:270"
+        	});
 
 		},
 

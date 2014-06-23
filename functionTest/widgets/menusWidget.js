d20f4ec34fca708e81b344a5dd6a42e032d7392f
atweb-project <dikostaras@yahoo.gr>
2014-06-24 01:41:46 +0300

    menu widget with smart menu

--- functionTest/widgets/menusWidget.js
+++ functionTest/widgets/menusWidget.js
@@ -6,121 +6,21 @@
         
         MyWidget.call(this, pid, ptype)
 
-      
+        var VertiCal = false
+        var NewItemTxt = ''
 
         this.createElement= function (param) {
 
-            var buttonText = 'Button!'
-
-
             if (typeof (param) !== 'undefined') {
 
-                if (typeof (param.text) !== 'undefined') buttonText = param.text
+                if (typeof (param.VertiCal) !== 'undefined') VertiCal = param.VertiCal
+                
+                if (typeof (param.NewItemTxt) !== 'undefined') NewItemTxt = param.NewItemTxt
 
             }
 
-           
-            return'<div id="' + this.getId()  +  '" style="width:100%; height:100%;"><ul class="sm sm-blue" id="main-menu">'+
-            			'<li class="current">'+
-        				'<a href="followed.html">menu item 1</a>'+
-        				'<ul>'+
-        					'<li>'+
-        						'<a href="followed.html">menu item</a>'+
-        					'</li>'+
-        					'<li class="current">'+
-        						'<a href="followed.html">long menu item sets sub width</a>'+
-        						'<ul>'+
-        							'<li class="current"><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        						'</ul>'+
-        					'</li>'+
-        					'<li>'+
-        						'<a href="followed.html">menu item</a>'+
-        						'<ul>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        						'</ul>'+
-        					'</li>'+
-        					'<li>'+
-        						'<a href="followed.html">menu item</a>'+
-        						'<ul>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        						'</ul>'+
-        					'</li>'+
-        				'</ul>'+
-        			'</li>'+
-        			'<li>'+
-        				'<a href="followed.html">menu item 2</a>'+
-        			'</li>'+
-        			'<li>'+
-        				'<a href="followed.html">menu item 3</a>'+
-        				'<ul>'+
-        					'<li>'+
-        						'<a href="followed.html">menu item</a>'+
-        						'<ul>'+
-        							'<li><a href="followed.html">short</a></li>'+
-        							'<li><a href="followed.html">short</a></li>'+
-        							'<li><a href="followed.html">short</a></li>'+
-        							'<li><a href="followed.html">short</a></li>'+
-        							'<li><a href="followed.html">short</a></li>'+
-        						'</ul>'+
-        					'</li>'+
-        					'<li>'+
-        						'<a href="followed.html">menu item</a>'+
-        						'<ul>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        						'</ul>'+
-        					'</li>'+
-        					'<li>'+
-        						'<a href="followed.html">menu item</a>'+
-        						'<ul>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        						'</ul>'+
-        					'</li>'+
-        					'<li>'+
-        						'<a href="followed.html">menu item</a>'+
-        						'<ul>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        						'</ul>'+
-        					'</li>'+
-        					'<li>'+
-        						'<a href="followed.html">menu item</a>'+
-        						'<ul>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        							'<li><a href="followed.html">menu item</a></li>'+
-        						'</ul>'+
-        					'</li>'+
-        				'</ul>'+
-        			'</li>'+
-        			'<li>'+
-        				'<a href="followed.html">menu item 4</a>'+
-        			'</li>'+	
-        		'</ul></div>'			
+            return '<iframe id="'+ this.getId() +'" width="100%" height="100%" src="menu/menuWidget.html" frameborder="0"'+
+            	   'verticalmenu="'+VertiCal+'" newitem="'+NewItemTxt+'"></iframe>'
 
         },
         
@@ -129,58 +29,76 @@
         	var example = $('#example').superfish({
 				//add options here if required
 			});*/
-        	if ((appGlobals.isInDesignMode() == false))
+        	/*if ((appGlobals.isInDesignMode() == false))
         	$('#main-menu').smartmenus();
-        	
+        	*/
 		},
+		
+		this.propChange = function (param) {
+			
+			for (var i = 0; i < param.length; i++) {
+
+				if (param[i].prop == 'VertiCal') VertiCal = param[i].value 
+				if (param[i].prop == 'NewItemTxt') NewItemTxt = param[i].value 
+					
+				}
+			
+		//	if(Vertical == true){
+				// for vertical direction we change the dimensions of the
+				// container
+			//	$('.elementContainer').css({
+				//	width:'135',
+				//	height: '345'					
+			//	})
+				
+		//	}				
+				
+				$('#'+this.getId() ).attr( 'verticalmenu', VertiCal)
+				$('#'+this.getId() ).attr( 'newitem', NewItemTxt)
+				
+				$('#'+this.getId() ).attr( 'src', function ( i, val ) { return val; });
+
+	    },
+	    
+	    this.selectionChanged = function()  {
+
+	    		$('#verticalmenu').prop('checked', VertiCal)
+	    		$('#newItem').prop('value', NewItemTxt)
+	              
+
+	    },
+	    
+	    this.chooseMenuLayout = function() {
+	    	
+	    	this.myRegisterUniquePropEvent([ {'prop' : 'VertiCal', 'ov' : VertiCal, 'nv' : $('#verticalmenu').prop('checked')} ])
+	    	
+	    },
+	    
+	    this.addNewItem = function() {
+	    	
+	    	this.myRegisterUniquePropEvent( [{ 'prop': 'NewItemTxt', 'ov': NewItemTxt, 'nv': $('#newItem').prop('value')}])
+	    	
+	    },
 
         this.createJSON = function() {
+	    	
+	      //alert ("json NewItemTxt " + NewItemTxt )
 
-            return { 'text': $('#'+ this.getId() ).html() }
-
-        },
-        
-        this.changeLabel= function()
-         {
-
-         //  alert ("button clicked")
-           // alert (  ' currentId '+currentID+" texregisterUniquePropEventt "+ $('#newButtonText').prop('value') )
-
-            this.myRegisterUniquePropEvent( [{ 'prop': 'text', 'ov': $('#' + this.getId()  ).html(), 'nv': $('#newButtonText').prop('value') }])
-             
-
-        },
-
-        this.propChange= function (param) {
-
-           // alert ("button prop change "+this.getId()+" param "+param.length)
-
-            $('#' + this.getId() ).html(param[0].value)
+            return { 'VertiCal': VertiCal, 'NewItemTxt': NewItemTxt }
 
         }
-
-
-        ,
-
-        this.selectionChanged=function()  {
-
-          //  alert(" button selection changed " + $(currentID).html())
-
-            $('#newButtonText').prop('value', $('#' + this.getId()  ).html())
-              
-
-         }
-        
+                   
     }
 
 // static variables/functions
 
 MenusWidget.init = function () {
-    $("#menusMenu").append("  Text:<input type='text' id='newButtonText'><button onclick='appGlobals.currentObject().changeLabel()'>Update</button>")
+    $("#menusMenu").append("<br>Choose vertical layout of the menu(default is horizontal)<input type='checkbox' id='verticalmenu' name='verticalmenu' value='' onclick='appGlobals.currentObject().chooseMenuLayout()'>")
+    $("#menusMenu").append("<br>Add new menu item<input type='text' id='newItem'><button onclick='appGlobals.currentObject().addNewItem()'>Add</button>")
 }
 MenusWidget.buttomImage='images/button_icon.png'
 MenusWidget.typeId= 'menus'
 MenusWidget.myClass= 'widget_menus'
-MenusWidget.initialWidth='454'
-MenusWidget.initialHeight= '50'
+MenusWidget.initialWidth='550'
+MenusWidget.initialHeight= '300'
 MenusWidget.actionsSectionId='menusMenu'

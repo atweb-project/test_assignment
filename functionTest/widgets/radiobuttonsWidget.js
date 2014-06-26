d617f34f62d65b1a415e7811195652fbfa41ff6e
Dimitris Kostaras <dk@out-there-media.com>
2014-06-26 17:54:45 +0300

    radio buttons continue

--- functionTest/widgets/radiobuttonsWidget.js
+++ functionTest/widgets/radiobuttonsWidget.js
@@ -1,103 +1,109 @@
-
-
-    
     function RadiobWidget (pid, ptype ){
 
         
         MyWidget.call(this, pid, ptype)
         
-        
+        var initialString = 'man,woman,child'
+        	
         var radioChecked = false
       
 
         this.createElement= function (param) {
 
-            var buttonText = 'Button!'
-
-
             if (typeof (param) !== 'undefined') {
+            	
+            	if (typeof (param.initialString) !== 'undefined') initialString = param.initialString
 
             	if (typeof (param.radioChecked) !== 'undefined') radioChecked = param.radioChecked
 
             }
-
-           
-            return '<div id="' + this.getId()  +  '" class="one" style="width:100%; height:100%">'+
-            	   '<input type="radio" name="family" value="man">Man<br><input type="radio" name="family" value="woman">Woman<br>'+
-            	   '<input type="radio" name="family" value="child">Child<br></div>'
+             
+            return '<div id="' + this.getId()  +  '" class="one" style="width:100%; height:100%"></div>'
 
         },
+        
         this.initElement = function() {
         	
-        	$('#'+this.getId()+'input[type=radio]').prop('checked',radioChecked)
-        	alert($('#'+this.getId()+' input[type=radio]').prop('value'))
+        	var Options = initialString.split(",")
+        	
+        	for (var i = 0; i < Options.length; i++) {
+        		
+        		$('#'+this.getId() ).append("<input type='radio' name='' value='"+Options[i]+"'>"+Options[i]+"<br>")
+        	}
+        	
+        	$('#'+this.getId()+' input[type=radio][value='+radioChecked+']').prop('checked', true)
+        	//alert($('#'+this.getId()+' input[type=radio]').prop('value'))
+        	//alert(radioChecked)
         },
 
         this.createJSON = function() {
 
-            return { 'radioChecked': radioChecked }
+            return { 'initialString': initialString, 'radioChecked': radioChecked }
 
         },
         
-        this.changeLabel= function()
-         {
-
-         //  alert ("button clicked")
-           // alert (  ' currentId '+currentID+" texregisterUniquePropEventt "+ $('#newButtonText').prop('value') )
-        	//alert($('#newButtonText').val())
+        this.radiobtnText = function() {
         	
+        	this.myRegisterUniquePropEvent( [{ 'prop': 'initialString', 'ov': initialString, 'nv': $('#newradiobtnText').prop('value') }])
+        },
+        
+        this.changeRadioCheckedBtn = function()
+         {
+   	
             this.myRegisterUniquePropEvent( [{ 'prop': 'radioChecked', 'ov': radioChecked, 'nv': $('#newradiobtn').prop('value') }])
-             
-
+            
+            // this.myRegisterUniquePropEvent( [{ 'prop': 'radioChecked', 'ov': radioChecked, 'nv': $('#newradiobtn').prop('checked') }])
+            
+        },
+        
+        this.nValues = function() {
+        	$('#newradiobtn').prop('value')
+        	$('#newradiobtn').prop('checked')
         },
         
         this.checkradioisChecked = function() {
         	
-        	/*var radio = $('#'+this.getId()+'input[type=radio]')
-            isChecked = radio.is(':checked')
-            
-		        if (isChecked) {
-		        	radio.prop('checked', false);
-
-		        } else {
-		        	radio.prop('checked', true);
-		        }*/
         	var newValue = $('#newradiobtn').prop('value')
-        	
-        	var RadioValue =$("input[type=radio][value="'+newValue+'"]")
-        	
-        	if(RadioValue && newValue == RadioValue)
-        		//$('#'+this.getId()+' input[type=radio]').val('newValue').attr("checked", "checked")
-        		$("input[type=radio][value="+newValue+"]").attr('checked','checked');
-        		alert(RadioValue)
+        	if($('#'+this.getId()+' input[type=radio][value='+newValue+']').length > 0)
+        		//alert('yes')
+        		$('#'+this.getId()+' input[type=radio][value='+newValue+']').prop("checked",true)
         		
         	
         },
 
         this.propChange= function (param) {
-
-           // alert ("button prop change "+this.getId()+" param "+param.length)
-
-        	/*var initialString = $('#newButtonText').val()
+        	
+        	for (var i = 0; i < param.length; i++) {
+        		
+        		if (param[i].prop == 'initialString') initialString = param[i].value 
+        		
+        		if (param[i].prop == 'radioChecked') radioChecked = param[i].value 
+        	}
+        	//alert($('#newradiobtn').prop('value'))
+        	
+        	
         	var Options = initialString.split(",")
+        	
+        	$('#'+this.getId()).contents().remove()
+        	
         	for (var i = 0; i < Options.length; i++) {
-        		$(".one" ).append("<input type='radio' name='' value='"+Options[i]+"'>"+Options[i]+"<br>")
-        	}*/
-        	alert($('#newradiobtn').prop('value'))
+        		      		
+        		$('#'+this.getId()).append("<input type='radio' name='' value='"+Options[i]+"'>"+Options[i]+"<br>")
+        	}
+        	
         	this.checkradioisChecked()
 
-        }
-
-
-        ,
+        },
 
         this.selectionChanged=function()  {
-
-          //  alert(" button selection changed " + $(currentID).html())
+        	var newValue = $('#newradiobtn').prop('value')
+        	
+        	$('#newradiobtnText').prop('value', initialString)
 
         	$('#newradiobtn').prop('value', radioChecked)
+        	$('#newradiobtn').attr('checked', newValue)
+        	//$('#'+this.getId()+' input[type=radio][value='+newValue+']').attr('checked', radioChecked)
               
-
          }
         
     }
@@ -105,18 +111,12 @@
 // static variables/functions
 
 RadiobWidget.init = function () {
-    $("#radiobuttonsMenu").append("Options:<input type='text' id='newradiobtn'><button onclick='appGlobals.currentObject().changeLabel()'>Update</button>")
+	$("#radiobuttonsMenu").append("Choose radio button names(please put names seperated by comma):<input type='text' id='newradiobtnText'><button onclick='appGlobals.currentObject().radiobtnText()'>Update</button>")
+    $("#radiobuttonsMenu").append("<br>Choose which radio button to preselect:<input type='text' id='newradiobtn'><button onclick='appGlobals.currentObject().changeRadioCheckedBtn()'>Update</button>")
 }
 RadiobWidget.buttomImage='images/button_icon.png'
-RadiobWidget.typeId= 'radiobuttons'
+RadiobWidget.typeId= 'radiobtns'
 RadiobWidget.myClass= 'widget_radiobuttons'
 RadiobWidget.initialWidth='100'
 RadiobWidget.initialHeight= '100'
 RadiobWidget.actionsSectionId='radiobuttonsMenu'
-
-// not actually  needed??
-//ButtonWidget.prototype = MyWidget
-
-//ButtonWidget.prototype = new MyWidget()
-// buttonwidget contructor is not changed
-//ButtonWidget.constructor = MyWidget()

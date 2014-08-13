7c02f05dab548991953903f5a4c063e7b98d5fcf
Dimitris Kostaras <dk@out-there-media.com>
2014-08-13 17:32:26 +0300

    restructuring_combine facebook widget

--- functionTest/widgets/facebookWidget.js
+++ functionTest/widgets/facebookWidget.js
@@ -1,6 +1,6 @@
 
 /*
- * Facebook Buttons Widget
+ * Facebook PLugins Widget
  */
 
 function FacebookBtnWidget (pid, ptype)
@@ -10,19 +10,20 @@ function FacebookBtnWidget (pid, ptype)
      
      	var FbPlugin = "buttons"
         
-        var url = "https://developers.facebook.com/docs/plugins/"
+      //FB buttons plugin
+     	var url = "https://developers.facebook.com/docs/plugins/"
      	var Layout = "standard"
         var ActionType = "like"
         var Share = true
         var Showfaces = true
         
-        var Showposts = false
-        var Showcomments = false
-        
+
+    //FB posts plugin    
         var urlP = "https://www.facebook.com/FacebookDevelopers/posts/10151471074398553"
         
+   //FB comments plugin
         var urlC = "http://example.com/comments"
-        var NumPosts = 5
+        var NumPosts = 4
         var ColorScheme = "light"
         	
 
@@ -43,20 +44,25 @@ function FacebookBtnWidget (pid, ptype)
                 if (typeof (param.Share) !== 'undefined') Share = param.Share
 
                 if( typeof (param.Showfaces) !== 'undefined') Showfaces = param.Showfaces
+                           
+                if( typeof (param.urlP) !== 'undefined') urlP = param.urlP
                 
-                if( typeof (param.Showposts) !== 'undefined') Showposts = param.Showposts
+                if( typeof (param.urlC) !== 'undefined') urlC = param.urlC
                 
-                if( typeof (param.Showcomments) !== 'undefined') Showcomments = param.Showcomments
+                if( typeof (param.NumPosts) !== 'undefined') NumPosts = param.NumPosts
+                
+                if( typeof (param.ColorScheme) !== 'undefined') ColorScheme = param.ColorScheme
 
                 //alert(" param.url "+param.url+"  param.allowFullscreen " + param.allowFullscreen + " number of params " + param.length)
 
              }
             
 
-            return '<iframe width="100%" id="' + this.getId() + '" class="fbbuttons" height="100%" src="//www.facebook.com/plugins/like.php?href='+url+'&amp;width&amp;layout='+Layout+'&amp;action='+ActionType+'&amp;show_faces='+Showfaces+'&amp;share='+Share+'&amp;" scrolling="no" frameborder="0" allowTransparency="true"></iframe>'+
-            	   '<iframe width="100%" id="' + this.getId() + '" class="fbposts" style="display:none;" height="100%" src="facebook/facebookpostsWidget.html" scrolling="no" frameborder="0" allowTransparency="true" urlp='+urlP+'></iframe>'+
-            	   '<iframe width="100%" id="' + this.getId() + '" class="fbcomments"  style="display:none;" height="100%" src="facebook/facebookcommentsWidget.html" scrolling="no" frameborder="0" '+
-                   'allowTransparency="true" urlc="'+urlC+'" numposts="'+NumPosts+'" colorscheme="'+ColorScheme+'"></iframe>'
+            return '<div  id="' + this.getId() + '" style="width:100%; height:100%;">'+
+            	   '<iframe width="100%" id="fbbuttons-' + this.getId() + '" height="100%" src="//www.facebook.com/plugins/like.php?href='+url+'&amp;width&amp;layout='+Layout+'&amp;action='+ActionType+'&amp;show_faces='+Showfaces+'&amp;share='+Share+'&amp;" scrolling="no" frameborder="0" allowTransparency="true"></iframe>'+
+            	   '<iframe width="100%" id="fbposts-' + this.getId() + '" style="display:none;" height="100%" src="facebook/facebookpostsWidget.html" scrolling="no" frameborder="0" allowTransparency="true" urlp='+urlP+'></iframe>'+
+            	   '<iframe width="100%" id="fbcomments-' + this.getId() + '"  style="display:none;" height="100%" src="facebook/facebookcommentsWidget.html" scrolling="no" frameborder="0" '+
+                   'allowTransparency="true" urlc="'+urlC+'" numposts="'+NumPosts+'" colorscheme="'+ColorScheme+'"></iframe></div>'
             
             
            
@@ -80,17 +86,41 @@ function FacebookBtnWidget (pid, ptype)
 
                 if( param[i].prop == 'Showfaces') Showfaces = param[i].value
                 
-                if( param[i].prop == 'Showposts') Showposts = param[i].value
+                if( param[i].prop == 'urlP') urlP = param[i].value
                 
-                if( param[i].prop == 'Showcomments') Showcomments = param[i].value
+                if( param[i].prop == 'urlC') urlC = param[i].value
+                
+                if( param[i].prop == 'NumPosts') NumPosts = param[i].value
+                
+                if( param[i].prop == 'ColorScheme') ColorScheme = param[i].value
 
 
             }
             
-            //alert(Share)
+            //alert(urlP)
+            if ((appGlobals.isInDesignMode() == true) && ( FbPlugin == 'buttons' )) {
+            	
+            	$('#fbbuttons-' + this.getId()).attr( 'src',  '//www.facebook.com/plugins/like.php?href='+url+'&width&layout='+Layout+'&action='+ActionType+'&show_faces='+Showfaces+'&share='+Share+'&;' )
+            
+            }
+            
+             if ((appGlobals.isInDesignMode() == true) && ( FbPlugin == 'posts' )) {
+            	
+            	$('#fbposts-' + this.getId()).attr( 'urlp', urlP)  
+            	$('#fbposts-' + this.getId()).attr( 'src', function ( i, val ) { return val; })
+            	
+             }
+         
+             if ((appGlobals.isInDesignMode() == true) && ( FbPlugin == 'comments' )) {
+            	 
+            	 $('#fbcomments-' + this.getId()).attr( 'urlc', urlC) 
+            	 $('#fbcomments-' + this.getId()).attr( 'numposts', NumPosts)
+            	 $('#fbcomments-' + this.getId()).attr( 'colorscheme', ColorScheme)
+            
+            	 $('#fbcomments-' + this.getId()).attr( 'src', function ( i, val ) { return val; })
+            	 
+             }
 
-            $('#'+this.getId() ).attr( 'src',  '//www.facebook.com/plugins/like.php?href='+url+'&width&layout='+Layout+'&action='+ActionType+'&show_faces='+Showfaces+'&share='+Share+'&;' )
-                      
         },
 
        this.selectionChanged = function()  {
@@ -109,79 +139,145 @@ function FacebookBtnWidget (pid, ptype)
             
             $('#showfaces').prop('checked', Showfaces )
             
-            $('#showposts').prop('checked', Showposts )
+            $('#newFacebookPostsURLText').prop('value', urlP )
             
-            $('#showcomments').prop('checked', Showcomments )
+            $('#newFacebookCommentsURLText').prop('value', urlC )
             
-            if($('#showposts').is(':checked')){
-            	
-            	$('#fbbuttons').toggle()
+            $('#comments').prop('value', NumPosts )
             
-            	$('.fbbuttons').hide()
-            	
-            	$('.fbcomments').hide()
-            	
-            	$('.fbposts').show()
-            
-            } 
-            /*else {
-            	
-            	$('.fbposts').hide()
-            	
-            	$('.fbcomments').hide()
-            	
-            	$('.fbbuttons').show()
-            	
-            }*/
-            
-            if($('#showcomments').is(':checked')){
-            	
-            	$('#fbbuttons').toggle()
-            
-            	$('.fbbuttons').hide()
-            	
-            	$('.fbposts').hide()
-            	
-            	$('.fbcomments').show()
-            
-            } 
-            /*else {
-            	
-            	$('.fbcomments').hide()
-            	
-            	$('.fbposts').hide()
-            	
-            	$('.fbbuttons').show()
-            	
-            }*/
+            $('#getcolor').prop('value', ColorScheme)
+                      
+            var PluginValue = FbPlugin 
+        	
+        	switch (PluginValue) { 
+		     case 'buttons' : 
+
+		    	 	$('#fbbtn').show()
+	                
+	            	$('#fbbuttons-' + this.getId()).show()
+	            	
+	            	$('#fbcomments-' + this.getId()).hide()
+	            	
+	            	$('#fbcomments').hide()
+	            	
+	            	$('#fbposts-' + this.getId()).hide()
+	            	
+	            	$('#fbposts').hide()
+	            	
+	            	// container
+	    				$('container' + this.elementNum).css({
+	    					width:'200',
+	    					height: '100',
+	    					background:'red'
+	    				})
+		    	 alert('container' + this.elementNum)
+		         break;
+		     case 'posts' : 
+		    	 
+		    	 	$('#fbbtn').hide()
+	                
+	            	$('#fbbuttons-' + this.getId()).hide()
+	            	
+	            	$('#fbcomments-' + this.getId()).hide()
+	            	
+	            	$('#fbcomments').hide()
+	            	
+	            	$('#fbposts-' + this.getId()).show()
+	            	
+	            	$('#fbposts').show()
+	            	
+    				// container
+	    				$('.elementContainer').css({
+	    					width:'400',
+	    					height: '350'					
+	    				})
+	    				
+			 
+		         break;
+		     case 'comments' : 
+		    	 
+		    	 	$('#fbbtn').hide()
+	                
+	            	$('#fbbuttons-' + this.getId()).hide()
+	            	
+	            	$('#fbcomments-' + this.getId()).show()
+	            	
+	            	$('#fbcomments').show()
+	            	
+	            	$('#fbposts-' + this.getId()).hide()
+	            	
+	            	$('#fbposts').hide()
+		    	 
+	            	// container
+	    				$('.elementContainer').css({
+	    					width:'400',
+	    					height: '380'					
+	    				})
+	            	
+		         break;      
+        	}
+           
 
        },
 
        this.initElement = function()
        {
-    	   
+    	   if ((appGlobals.isInDesignMode() == false))
+    		   
+    		   var PluginPreviewValue = FbPlugin 
+           	
+           	switch (PluginPreviewValue) { 
+   		     case 'buttons' : 
+	 	 	                
+   	            	$('#fbbuttons-' + this.getId()).show()
+   	            	
+   	            	$('#fbcomments-' + this.getId()).hide()
+   	            	
+   	            	$('#fbposts-' + this.getId()).hide()
+   	            	   		    	 
+   		         break;
+   		     case 'posts' : 
+   		    	 	                
+   	            	$('#fbbuttons-' + this.getId()).hide()
+   	            	
+   	            	$('#fbcomments-' + this.getId()).hide()
+   	            	
+   	            	$('#fbposts-' + this.getId()).show()
+   	            	
+   		         break;
+   		     case 'comments' : 
+   		    	 
+   	            	$('#fbbuttons-' + this.getId()).hide()
+   	            	
+   	            	$('#fbcomments-' + this.getId()).show()
+   	            	
+   	            	$('#fbposts-' + this.getId()).hide()
+   	            	
+   			
+   		         break;      
+           	}
             
             	
-        },
+       },
         
-        this.changeURL = function ()
-        {
+       this.changeURL = function ()
+       {
             this.myRegisterUniquePropEvent(  [{ 'prop': 'url', 'ov': url, 'nv': $('#newFacebookURLText').prop('value') }])
 
-        },
+       },
 
 
-        this.getLayout = function()
-        {
+       this.getLayout = function()
+       {
         	this.myRegisterUniquePropEvent(  [{ 'prop': 'Layout', 'ov': Layout, 'nv': $('#layout').prop('value') }])
 
-        },
+       },
         
-        this.getactiontype = function()
-        {
+       this.getactiontype = function()
+       {
         	this.myRegisterUniquePropEvent(  [{ 'prop': 'ActionType', 'ov': ActionType, 'nv': $('#actiontype').prop('value') }])
 
-        },
+       },
 
 
         this.showShareButton = function ()
@@ -197,51 +293,43 @@ function FacebookBtnWidget (pid, ptype)
              
             this.myRegisterUniquePropEvent(  [{ 'prop': 'Showfaces', 'ov': Showfaces, 'nv': $('#showfaces').prop('checked') }])
 
-         },
+        },
          
-        this.showPosts = function ()
+        this.changeURLP = function ()
         {
-            $('#fbbtn').toggle()
-            
-            if (Showposts = true){
-            	Showcomments = false
-            	$('#showcomments').removeAttr('checked')
-            }
-            this.myRegisterUniquePropEvent(  [{ 'prop': 'Showposts', 'ov': Showposts, 'nv': $('#showposts').prop('checked') }])
-            
-            
+             this.myRegisterUniquePropEvent(  [{ 'prop': 'urlP', 'ov': urlP, 'nv': $('#newFacebookPostsURLText').prop('value') }])
+
+        },
+         
+        this.changeURLC = function ()
+        {
+            this.myRegisterUniquePropEvent(  [{ 'prop': 'urlC', 'ov': urlC, 'nv': $('#newFacebookCommentsURLText').prop('value') }])
+
+        }, 
+        
+        this.getCommentsNum = function()
+        {
+        	this.myRegisterUniquePropEvent(  [{ 'prop': 'NumPosts', 'ov': NumPosts, 'nv': $('#comments').prop('value') }])
 
         },
         
-        this.showComments = function ()
+        this.getcolor = function()
         {
-            $('#fbbtn').toggle()
-            
-            if (Showcomments = true){
-            	Showposts = false
-            	$('#showposts').removeAttr('checked')
-            }
-            
-            this.myRegisterUniquePropEvent(  [{ 'prop': 'Showcomments', 'ov': Showcomments, 'nv': $('#showcomments').prop('checked') }])
-            
-           
+        	this.myRegisterUniquePropEvent(  [{ 'prop': 'ColorScheme', 'ov': ColorScheme, 'nv': $('#getcolor').prop('value') }])
 
         },
         
         this.getPlugin = function ()
         {
-          
-            
-            this.myRegisterUniquePropEvent(  [{ 'prop': 'FbPlugin', 'ov': FbPlugin, 'nv': $('#fbplugin').prop('checked') }])
-            
-           
+                    
+            this.myRegisterUniquePropEvent(  [{ 'prop': 'FbPlugin', 'ov': FbPlugin, 'nv': $('#fbplugin').prop('value') }])
 
        },
 
 
        this.createJSON = function () {
          
-            return { 'url': url,'Layout': Layout,'ActionType': ActionType, 'Share': Share, 'Showfaces': Showfaces, 'Showposts': Showposts, 'Showcomments': Showcomments } 
+            return { 'FbPlugin': FbPlugin, 'url': url,'Layout': Layout,'ActionType': ActionType, 'Share': Share, 'Showfaces': Showfaces, 'urlP': urlP, 'urlC': urlC, 'NumPosts':NumPosts, 'ColorScheme': ColorScheme } 
         }
         
 
@@ -257,7 +345,7 @@ FacebookBtnWidget.initialHeight= '100'
 FacebookBtnWidget.actionsSectionId= 'facebookMenu'
 
 
-FacebookBtnWidget.init= function () {
+FacebookBtnWidget.init = function () {
 	
 	$("#facebookMenu").append(
 
@@ -286,10 +374,16 @@ FacebookBtnWidget.init= function () {
     
     							"<br>Show Friend's faces<input type='checkbox' id='showfaces' onclick='appGlobals.currentObject().showFaces()'></div>")
     
-    $("#facebookMenu").append("<br>Show Facebook posts<input type='checkbox' id='showposts' onclick='appGlobals.currentObject().showPosts()'>")
+    $("#facebookMenu").append("<div id='fbposts'>URL<input type='text' id='newFacebookPostsURLText'><button onclick='appGlobals.currentObject().changeURLP()'>Update</button></div>")
     
-    $("#facebookMenu").append("<br>Show Facebook comments<input type='checkbox' id='showcomments' onclick='appGlobals.currentObject().showComments()'>")
+    $("#facebookMenu").append("<div id='fbcomments'>URL<input type='text' id='newFacebookCommentsURLText'><button onclick='appGlobals.currentObject().changeURLC()'>Update</button>"+
+    						  "<br>Choose the number of comments to show <input type='edit' id='comments' name='comments' value='' >"+
+    						  "<div>Choose the color of the scheme<select id='getcolor' onchange='appGlobals.currentObject().getcolor()'>"+
+    						  "<option value='light'>light</option>"+
+    						  "<option value='dark'>dark</option>"+
+    						  "</select></div></div>")
 
+    $("#comments").spinner({ min: 1, change: function (event, ui) { if (event.originalEvent) appGlobals.currentObject().getCommentsNum() } })
 
 }
 

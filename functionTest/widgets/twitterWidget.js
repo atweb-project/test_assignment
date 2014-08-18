836cce5278166fbdc4f6b65e11d467798cff7664
Dimitris Kostaras <dk@out-there-media.com>
2014-08-18 16:31:29 +0300

    twitter widget restructure start

--- functionTest/widgets/twitterWidget.js
+++ functionTest/widgets/twitterWidget.js
@@ -6,31 +6,55 @@
 function TwitterBtnWidget (pid, ptype)
 {
        
-     MyWidget.call(this,pid,ptype)       
-        
+     MyWidget.call(this,pid,ptype)   
+     
+     	var TwPlugin = "buttons"
+       
+     //Twitter buttons plugin
         var url = "http://example.com/comments"
      	var Count = "horizontal"
         
-
+    //Twitter feed plugin 		
+     	var urlF = "133640144317198338"
+    
+    //Twitter follow plugin
+     	var urlFo = "https://twitter.com/twitterapi"
+     	var User = "@twitterapi"
+     	var showUserName = true
+     	var Show_Count = true
 
        // alert (" this.id is "+ this.getId() )
         this.createElement = function (param) {
    
             
             if( typeof(param) !== 'undefined') {
+            	
+            	if( typeof (param.TwPlugin) !== 'undefined') TwPlugin = param.TwPlugin
 
                 if ( typeof (param.url) !== 'undefined') url = param.url
 
                 if (typeof (param.Count) !== 'undefined') Count = param.Count
+                
+                if (typeof (param.urlF) !== 'undefined') urlF = param.urlF
+                
+                if (typeof (param.urlFo) !== 'undefined') urlFo = param.urlFo
+                
+                if (typeof (param.User) !== 'undefined') User = param.User
+                
+                if (typeof (param.showUserName) !== 'undefined') showUserName = param.showUserName
+                
+                if (typeof (param.Show_Count) !== 'undefined') Show_Count = param.Show_Count
 
                    //alert(" param.url "+param.url+"  param.allowFullscreen " + param.allowFullscreen + " number of params " + param.length)
 
              }
               
 
-            return '<iframe width="100%" id="' + this.getId() + '" height="100%" src="twitter/twitterbtnWidget.html" scrolling="no" frameborder="0" allowTransparency="true"'+
-            'url="'+url+'" count="'+Count+'"></iframe>'
-            
+            return '<div  id="' + this.getId() + '" style="width:100%; height:100%;">'+
+            	   '<iframe width="100%" id="twbuttons-' + this.getId() + '" height="100%" src="twitter/twitterbtnWidget.html" scrolling="no" frameborder="0" allowTransparency="true" url="'+url+'" count="'+Count+'"></iframe>'+
+            	   '<iframe width="100%" id="twfeeds-' + this.getId() + '" height="100%" src="twitter/twitterfeedWidget.html" scrolling="no" frameborder="0" allowTransparency="true" url="'+urlF+'"></iframe>'+
+            	   '<iframe width="100%" id="twfollow-' + this.getId() + '" height="100%" src="twitter/twitterfollowbtnWidget.html" scrolling="no" frameborder="0" allowTransparency="true"'+
+                   'url="'+urlFo+'" user="'+User+'" showusername="'+showUserName+'" showcount="'+Show_Count+'"></iframe>'
         },
 
         this.propChange = function (param) {
@@ -38,9 +62,20 @@ function TwitterBtnWidget (pid, ptype)
 
             for (var i = 0; i < param.length; i++) {
 
+                if (param[i].prop == 'TwPlugin') TwPlugin =  param[i].value 
+                
                 if (param[i].prop == 'url') url =  param[i].value 
                   
                 if (param[i].prop == 'Count') Count = param[i].value
+                
+                if (param[i].prop == 'urlF') urlF = param[i].value
+                
+                if (param[i].prop == 'urlFo') urlFo = param[i].value
+                
+                if (param[i].prop == 'showUserName') showUserName = param[i].value
+                
+                if (param[i].prop == 'Show_Count') Show_Count = param[i].value
+                   
 
             }
             
